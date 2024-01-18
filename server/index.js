b29d4589e6195afcb8afd3f0fca83ed04defdd58
Chhavi <guptachhavi880@gmail.com>
2024-01-19 02:44:25 +0530

    added login and register routes with jwttokenization

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ dotenv.config();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(bodyParser.json())
 app.get('/health', (req, res) => {
     res.send('Server is up and running.');
 });
+
+app.use('/user', authRoutes);
 
 app.listen(process.env.PORT, () => {
     mongoose.connect(process.env.MONGODB_URL)
